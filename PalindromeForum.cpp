b150c4f079bc7a54f771c311256f3509ad2a5466
niverred <anisa.zikra@gmail.com>
2022-03-28 22:20:52 +0700

    Update PalindromeForum.cpp

--- PalindromeForum.cpp
+++ PalindromeForum.cpp
@@ -1,35 +1,37 @@
 #include <iostream>
 using namespace std;
-int palindrome_check(string s, int start, int end)
+int palindrome_check(string s, int start, int end) //calling for function
 {
-    if(end - start == 1 || start == end)
+    if(end - start == 1 || start == end) //if the string length = 1, return true
     {
         return 1;
     }
-    else
+    else //else, compare the first and last characters, and check them
     {
-        if (s [start] == s [end])
+        if (s [start] == s [end]) //if both characters are the same, then apply recursion to the remaining substrings
         {
             return palindrome_check(s, start+1, end-1); 
         }
-        else
+        else //else return false
         {
             return 0;
         }
     }
 }
+
+//driver code
 int main()
 {
    string s;
-   cin >> s;
+   cin >> s; //input from user
    int n = s.length();
    if(palindrome_check (s, 0, n-1))
    {
-       cout << "Yes" << endl;
+       cout << "Yes" << endl; //output yes if the string is a palindrome
    }
    else
    {
-       cout << "No" << endl;
+       cout << "No" << endl; //output no if the string is not a palindrome
    }
    return 0;
 }
